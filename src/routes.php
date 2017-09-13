117985e0e3b704c6ea3514a7b2fe859ca1ba816b
Román Gorojovsky Sánchez <rgorojovsky@gmail.com>
2017-09-13 19:06:55 -0300

    Agrego ruteo a los métodos que se van a usar

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,39 @@
 <?php
+
+require_once('Controller/TasksApiController.php');
+
 // Routes
+$app->get('/test/', function ($request, $response, $args) {
+	return (new TasksApiController($request, $response, $this))->test();
+});
+
+$app->post('/create/', function ($request, $response, $args) {
+	return (new TasksApiController($request, $response, $this))->create();
+});
+
+$app->put('/update/{id}', function ($request, $response, $args) {
+	return (new TasksApiController($request, $response, $this))->update($args['id']);
+});
+
+$app->put('/mark_complete/{id}', function ($request, $response, $args) {
+	return (new TasksApiController($request, $response, $this))->mark_complete($args['id']);
+});
+
+$app->put('/mark_incomplete/{id}', function ($request, $response, $args) {
+	return (new TasksApiController($request, $response, $this))->mark_incomplete($args['id']);
+});
+
+$app->get('/task/{id}/', function ($request, $response, $args) {
+	return (new TasksApiController($request, $response, $this))->show($args['id']);
+});
+
+$app->get('/tasks/', function ($request, $response, $args) {
+	return (new TasksApiController($request, $response, $this))->list();
+});
+
+$app->delete('/delete/{id}/', function ($request, $response, $args) {
+	return (new TasksApiController($request, $response, $this))->delete($args['id']);
+});
 
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
@@ -8,3 +42,5 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+
